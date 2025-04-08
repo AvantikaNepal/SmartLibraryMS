15ccad3af675b336b6c45873f73faca5d452adbf
AvantikaNepal <navantika20@tbc.edu.np>
2025-04-08 20:54:52 +0545

    Added class user and library with methods in each class

--- SmartLibraryMS.py
+++ SmartLibraryMS.py
@@ -10,6 +10,35 @@ class Books:
         print('The book "'   + self.title + '" with the ISBN: ' + str(self.ISBN) + ' with the author: ' + self.author + ' has ' + str(self.available_copies) + ' available copies  left!')
 
 
+class User:
+    def __init__(self, name, user_id, borrowed_books):
+        self.name = name
+        self.user_id = user_id
+        self.borrowed_books = borrowed_books
 
-B1 = Books("The palace of illusion", 3343444, "Om Prakash", 34)
-B1.display_info()
+    def borrowed_book(self):
+        return ''
+
+    def user_info(self):
+        return ''
+
+    def return_book(self):
+        return ''
+
+class Library:
+    def __init__(self, book_list, user_list):
+        self.book_list = book_list
+        self.user_list = user_list
+
+    def add_book(self):
+        return ''
+    def remove_book(self):
+        return ''
+    def register_user(self):
+        return ''
+    def issue_book(self):
+        return ''
+    def return_book(self):
+        return ''
+    def display_all_books(self):
+        return ''
