3183c2b19151f598372bfef040c372396de30cf6
AvantikaNepal <navantika20@tbc.edu.np>
2025-04-18 14:40:57 +0545

    Book class file created

---
+++ Book.py
@@ -0,0 +1,48 @@
+class Book:
+    def __init__(self, title, ISBN, author, available_copies):
+        self.set_title(title)
+        self.set_isbn(ISBN)
+        self.set_author(author)
+        self.set_available_copies(available_copies)
+# getter functions
+    def get_title(self):
+        return self.__title
+    def get_isdn(self):
+        return self.__ISBN
+    def get_author(self):
+        return self.__author
+    def get_available_copies(self):
+        return self.__available_copies
+
+# setter functions
+    def set_title(self, title):
+        title = title.strip()  # Remove leading/trailing spaces
+        if not title.isalpha():  # Check if it contains only alphabetic characters
+            raise ValueError("Title should only contain alphabetic characters!")
+        elif title == "":
+            raise ValueError("Title cannot be empty!")
+        else:
+            self.__title = title
+
+    def set_isbn(self, isbn):
+        if isbn < 0:
+            raise ValueError("ISBN cannot be negetive")
+        else:
+            self.__ISBN = isbn
+    def set_author(self, author):
+        author = author.strip()  # Remove leading/trailing spaces
+        if not author.isalpha():  # Check if it contains only alphabetic characters
+            raise ValueError("Author name should only contain alphabetic characters!")
+        elif author == "":
+            raise ValueError("Author cannot be empty!")
+        else:
+            self.__author = author
+    def set_available_copies(self, available_copies):
+        if available_copies < 0:
+            raise ValueError("Available books cannot be negetive!")
+        else:
+            self.__available_copies = available_copies
+
+
+    def display_info(self):
+        print('The book "'   + self.__title + '" with the ISBN: ' + str(self.__ISBN) + ' with the author: ' + self.__author + ' has ' + str(self.__available_copies) + ' available copies  left!')
